894dda8ff30a7071255f7dc9544ca8ec7888b2e5
irog-c <mihajlov.igor15@gmail.com>
2024-06-20 15:20:43 +0200

    Add mocks for unit tests for verifier class

--- source/socket_wrapper.cpp
+++ source/socket_wrapper.cpp
@@ -3,12 +3,15 @@
 //
 
 #include <../include/socket_wrapper.hpp>
+#include <cstddef>
 #include <iostream>
+#include <stdexcept>
+#include "asio/buffer.hpp"
 
 using namespace tuposoft::vrf;
 using tcp = asio::ip::tcp;
 
-void socket_wrapper::connect(const std::string host, const std::string service) {
+void socket_wrapper::connect(const std::string &host, const std::string &service) {
     const auto query = tcp::resolver::query{host, service};
     auto resolver = tcp::resolver{io_context_};
     auto it = resolver.resolve(query);
@@ -25,8 +28,24 @@ void socket_wrapper::connect(const std::string host, const std::string service) 
     }
 }
 
-std::size_t socket_wrapper::read_until(char delimiter) { return {}; }
+std::size_t socket_wrapper::read(std::vector<std::byte> &buffer) {
+    auto ec = asio::error_code();
+    auto read_len = socket_.read_some(asio::buffer(buffer.data(), buffer.size()), ec);
+    if (ec) {
+        throw std::runtime_error(ec.message());
+    }
 
-std::size_t socket_wrapper::write(std::string message) { return {}; }
+    return read_len;
+}
 
-void socket_wrapper::close() {}
+std::size_t socket_wrapper::write(const std::string &message) {
+    auto ec = asio::error_code();
+    auto write_len = socket_.write_some(asio::buffer(message.data(), message.size()), ec);
+    if (ec) {
+        throw std::runtime_error(ec.message());
+    }
+
+    return write_len;
+}
+
+void socket_wrapper::close() { socket_.close(); }
